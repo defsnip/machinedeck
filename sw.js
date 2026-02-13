59d59bcd387018402f4377039213f930c19dd35e
defsnip <mailshaurya@proton.me>
2026-02-13 22:39:13 +0530

    initial commit

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'machine-deck-v1';
+const ASSETS = [
+  './',
+  './index.html', // Make sure your main file is named index.html
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
